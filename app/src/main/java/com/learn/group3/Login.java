5bd9c63658a95f7c442ef52853e13b1b4940f820
rifkatr13 <31616451+rifkatr13@users.noreply.github.com>
2017-09-09 22:22:48 +0700

    second

--- app/src/main/java/com/learn/group3/Login.java
+++ app/src/main/java/com/learn/group3/Login.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     EditText etName;
     Button bstart;
     RadioGroup rg;
+    RadioButton cMale, cFemale;
+    String gender = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +26,38 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etName = (EditText)findViewById(R.id.edName);
         bstart = (Button)findViewById(R.id.bStart);
         rg = (RadioGroup)findViewById(R.id.rgGender);
+        cMale = (RadioButton) findViewById(R.id.rbMale);
+        cFemale = (RadioButton) findViewById(R.id.rbFemale);
+
+        cMale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gender = "Male";
+            }
+        });
+
+        cFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gender = "Female";
+            }
+        });
 
         bstart.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this, Home.class);
-        intent.putExtra("edname",etName.getText().toString());
-        startActivity(intent);
-        finish();
+        if (!cMale.isChecked() && !cFemale.isChecked()){
+            Toast.makeText(Login.this, "You Must Choose a gender", Toast.LENGTH_SHORT).show();
+        }else if(etName.getText().toString().trim().equals("")){
+            Toast.makeText(Login.this, "You Must Insert a Name", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent intent = new Intent(this, Home.class);
+            intent.putExtra("gender", gender);
+            intent.putExtra("edname",etName.getText().toString());
+            startActivity(intent);
+            finish();
+        }
     }
 }
